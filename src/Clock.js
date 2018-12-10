fa6d93208f5e4aa5ecb6425531e053cba533121d
Olivier Valette <olivier.valette@spi10.com>
2018-12-10 13:57:59 +0100

    Clock component

--- src/Clock.js
+++ src/Clock.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
 
-class Clock extends Component{
-
+class Clock extends Component {
     constructor(props) {
         super(props);  // renvoie les attributs au parent
         this.state = { date: new Date() };
     }
-
+    componentDidMount() {
+        this.timer = setInterval(() => this.setState({ date: new Date() }), 1000);
+    }
+    componentWillUnmount() {
+        clearInterval(this.timer);
+    }
     render () {
         return (<p>Il est {this.state.date.toLocaleTimeString()}</p>);
     }
-
 }
-
 export default Clock;
